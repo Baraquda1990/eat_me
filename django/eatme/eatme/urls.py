f34e693195f739d2967bd6d413873534e0daf97d
Damir <damir.gilmanov2015@yandex.ru>
2026-05-16 12:17:18 +0500

    добавил djoser

--- django/eatme/eatme/urls.py
+++ django/eatme/eatme/urls.py
@@ -22,6 +22,8 @@ from drf_spectacular.views import (SpectacularAPIView,SpectacularSwaggerView,Spe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),

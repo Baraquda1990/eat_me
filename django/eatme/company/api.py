e368c25ea42bf7a74be5015c9b35d9473f931bea
Damir <damir.gilmanov2015@yandex.ru>
2026-05-22 13:29:58 +0500

    сортировка компаний по расстоянию

--- django/eatme/company/api.py
+++ django/eatme/company/api.py
@@ -1,9 +1,13 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView,ListAPIView
 from rest_framework.permissions import AllowAny
-
 from .models import Company
 from .serializers import CompanySerializer
 from drf_spectacular.utils import extend_schema
+from math import radians
+from django.db.models import (
+    F, Value, FloatField, ExpressionWrapper
+)
+from django.db.models.functions import ACos, Cos, Sin, Radians
 
 @extend_schema(
     description="Детальная информация о компании."
@@ -13,3 +17,32 @@ class CompanyDetail(RetrieveAPIView):
     permission_classes=[AllowAny]
     serializer_class=CompanySerializer
     lookup_field='slug'
+
+@extend_schema(
+    description="Список компаний. GET /company/?latitude=50.283&longitude=57.167 - сортировка по расстоянию"
+)
+class CompanyList(ListAPIView):
+    serializer_class = CompanySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        qs = Company.objects.all()
+        lat = self.request.query_params.get('latitude')
+        lon = self.request.query_params.get('longitude')
+        if not lat or not lon:
+            return qs
+        try:
+            lat = float(lat)
+            lon = float(lon)
+        except ValueError:
+            return qs
+        distance = 6371 * ACos(
+            Cos(Radians(Value(lat))) *
+            Cos(Radians(F('latitude'))) *
+            Cos(Radians(F('longitude')) - Radians(Value(lon))) +
+            Sin(Radians(Value(lat))) *
+            Sin(Radians(F('latitude')))
+        )
+        return qs.annotate(
+            distance=ExpressionWrapper(distance, output_field=FloatField())
+        ).order_by('distance')
--- django/eatme/company/urls.py
+++ django/eatme/company/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .api import CompanyDetail
+from .api import CompanyDetail,CompanyList
 '''
 Эндпоинт предоставления списка Каталога
 '''
 urlpatterns = [
+    path('company/', CompanyList.as_view()),
     path('company/<slug:slug>/',CompanyDetail.as_view())
 ]
